9342c30ea287818e0d537f9da113e9308871e432
CodeClan Student <codeclanstudent@MacBook-Pro-2.local>
2020-09-08 19:18:55 +0100

    first commit

---
+++ tests/calculator_test.py
@@ -0,0 +1,16 @@
+import unittest
+from src.calculator import add, divide, multiply, subtract
+
+class TestCalculator(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(5, add(2,3))
+
+    def test_subtract(self):
+        self.assertEqual(2, subtract(5,3))
+
+    def test_divide(self):
+        self.assertEqual(3, divide(9,3))
+
+    def test_multiply(self):
+        self.assertEqual(25, multiply(5,5))
